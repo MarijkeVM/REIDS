056347c186a751f2f24759f0ea0d92037ff6974d
MarijkeVM <MarijkeVM@users.noreply.github.com>
2017-10-18 14:39:33 +0200

    Update CreateOutput.R

--- REIDSModel/Parallel/CreateOutput.R
+++ REIDSModel/Parallel/CreateOutput.R
@@ -3,12 +3,12 @@ geneID=read.csv(file="geneID.csv",header=TRUE)
 CreateOutput<-function(ID){
 	Output=list()
 	for(i in ID$GeneID){
-		Data=get(load(paste("OutputGDS_geneID",as.character(i),".RData",sep="")))
+		Data=get(load(paste("OutputREIDS_geneID",as.character(i),".RData",sep="")))
 		
 		Output[length(Output)+1]=Data
 		names(Output)[length(Output)]=i
 	}
-	HTAData_RASA_WithoutJunctions_OutputGDSModel=Output
+	HTAData_RASA_WithoutJunctions_OutputREIDSModel=Output
 	save(HTAData_RASA_WithoutJunctions_OutputREIDSModel,file="../HTAData_RASA_WithoutJunctions_OutputREIDSModel.RData")
 	
 }
